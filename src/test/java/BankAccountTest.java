2383090496002608d6f10e42ce9ac68b16e342f3
Cesar David Millan <cesardavidmillan5@gmail.com>
2026-05-08 04:41:27 -0500

    completed unit test

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -9,11 +9,49 @@ public class BankAccountTest {
 
     @Test
     @DisplayName("Deposit into Savings Account")
-    void makeDeposit() {
+    void makeDepositSavingAccount() {
         SavingsAccount savingsAccountTest = new SavingsAccount();
-
         savingsAccountTest.deposit(BigDecimal.valueOf(15000.00));
-
         assertEquals(BigDecimal.valueOf(15000.00), savingsAccountTest.getBalance());
     }
+
+    @Test
+    @DisplayName("Deposit into Checking Account")
+    void makeDepositCheckingAccount() {
+        CheckingAccount checkingAccountTest = new CheckingAccount();
+        checkingAccountTest.deposit(BigDecimal.valueOf(15000.00));
+        assertEquals(BigDecimal.valueOf(15000.00), checkingAccountTest.getBalance());
+    }
+
+
+    @Test
+    @DisplayName("Withdraw from Savings Account")
+    void makeWithdrawSavingAccount() {
+        SavingsAccount savingsAccountTest = new SavingsAccount();
+        savingsAccountTest.deposit(BigDecimal.valueOf(15000.00));
+        savingsAccountTest.withdraw(BigDecimal.valueOf(15000.00));
+        assertEquals(BigDecimal.valueOf(0.00), savingsAccountTest.getBalance());
+    }
+
+
+    @Test
+    @DisplayName("Withdraw from Savings Account")
+    void makeWithdrawCheckingAccount() {
+        CheckingAccount checkingAccountTest = new CheckingAccount();
+        checkingAccountTest.deposit(BigDecimal.valueOf(15000.00));
+        checkingAccountTest.withdraw(BigDecimal.valueOf(15000.00));
+        assertEquals(BigDecimal.valueOf(0.00), checkingAccountTest.getBalance());
+    }
+
+
+    @Test
+    @DisplayName("Applying Interest into Savings Account")
+    void applyInterestRateSavingAccount() {
+        SavingsAccount savingsAccountTest = new SavingsAccount();
+        savingsAccountTest.deposit(BigDecimal.valueOf(15000.00));
+        savingsAccountTest.applyInterest();
+        assertEquals(BigDecimal.valueOf(0.00), savingsAccountTest.getBalance());
+    }
+
+
 }
